cc0bef6637fc6783a4c0b08582c48e50bfe77bed
brent morris <inkrypto@gmail.com>
2015-02-23 19:13:21 -0600

    Added Category Product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224010747) do
+ActiveRecord::Schema.define(version: 20150224011210) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categorized_products", force: :cascade do |t|
     t.integer  "product_id",  limit: 4
